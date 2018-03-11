3602e4882a7f21bb259e9a4c1d4aa705070f5e20
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-11 11:18:35 -0400

    add create into frontend

--- frontend.rb
+++ frontend.rb
@@ -6,7 +6,7 @@ response = Unirest.get("#{base_url}/contacts")
 
 puts JSON.pretty_generate(response.body)
 
-# show specific contact
+# ** show specific contact
 # specific_response = Unirest.get("#{base_url}/contacts/100")
 # puts JSON.pretty_generate(specific_response.body)
 
@@ -21,6 +21,17 @@ puts JSON.pretty_generate(response.body)
 
 # puts JSON.pretty_generate(update_response.body)
 
-# Delete contact
+# ** Delete contact
 # delete_response = Unirest.delete("#{base_url}/contacts/99")
 # puts JSON.pretty_generate(delete_response.body)
+
+# ** Create New Contact
+# input_first_name = "Just"
+# input_last_name = "Working"
+# input_email = "just@example.org"
+
+# create_response = Unirest.post("#{base_url}/contacts", parameters: {first_name: input_first_name, 
+#       last_name: input_last_name, 
+#       email: input_email})
+
+
