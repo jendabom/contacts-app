6e0daba7e7544c2149f3da9f4cc6883a8d01a935
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-11 13:32:12 -0400

    override as_json method for this app, and add additional models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,23 @@
 class Contact < ApplicationRecord
+  def as_json
+    { 
+      full_name: full_name,
+      email: email, 
+      phone_number: phone_number_with_japan_country_code, 
+      created_at: created_date_pretty
+    }
+  end
+
+  def created_date_pretty
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def phone_number_with_japan_country_code
+    japan_country_code = "+81"
+    "#{japan_country_code} #{phone_number}"
+  end
 end
