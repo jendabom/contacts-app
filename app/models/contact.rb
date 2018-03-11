a1a817c3e0f16972ac7bed533f3bd687d54a1cc8
Jennifer Blom <jennifer.blom@betterment.com>
2018-03-11 13:38:40 -0400

    add pretty updated time

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,12 +4,17 @@ class Contact < ApplicationRecord
       full_name: full_name,
       email: email, 
       phone_number: phone_number_with_japan_country_code, 
-      created_at: created_date_pretty
+      created_at: created_date_pretty,
+      updated_at: updated_date_pretty
     }
   end
 
   def created_date_pretty
     created_at.strftime("%b %d, %Y")
+  end
+
+  def updated_date_pretty
+    updated_at.strftime("%b %e %Y at%l:%M%p")
   end
 
   def full_name
